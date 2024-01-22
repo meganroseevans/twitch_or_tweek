28a8235177aa35db10471ad4527e4db762410b60
megan.evans <meganroseevans1@gmail.com>
2024-01-22 13:56:21 +1100

    convert game.py to function oriented

--- game.py
+++ game.py
@@ -1,82 +1,111 @@
 import streamlit as st
-import random
 import pandas as pd
+import random
+import requests
 
-def pick_new_birds(bird_images_df, mode):
-    # Select a random bird entry from the DataFrame
-    selected_bird = bird_images_df.sample().iloc[0]
-    bird_name, bird_image, bird_category = selected_bird['name'], selected_bird['image_id'], selected_bird['category']
+class BirdGame:
+    def __init__(self, bird_images_df):
+        self.bird_images_df = bird_images_df
+        self.score = 0
+        self.mode = 'hard mode'
+        self.selection = None
+        self.target_bird_name, self.target_bird_image, self.multichoice_options, self.correct_bird_index = self.new_game_state()
+        self.buttons = [self.multichoice_options]
 
-    # Determine other birds to display based on the game mode
-    if mode == 'easy mode':
-        other_birds = [x for x in bird_images_df.name.unique().tolist() if x != bird_name]
-    elif mode == 'hard mode':
-        other_birds = [x for x in bird_images_df[bird_images_df.category == bird_category].name.unique().tolist() if x != bird_name]
+    def get_target_bird(self):
+        selected_bird = self.bird_images_df.sample().iloc[0]
+        return selected_bird['name'], selected_bird['image_id'], selected_bird['category']
 
-    # Determine the number of buttons to display and shuffle the order
-    num_buttons = min(5, len(other_birds))
-    birds_to_display = [bird_name] + random.sample(other_birds, num_buttons)
-    random.shuffle(birds_to_display)
+    def get_multichoice_options(self, target_bird_name, target_bird_category):        
+        if self.mode == 'easy mode':
+            non_target_birds = [x for x in self.bird_images_df.name.unique().tolist() if x != target_bird_name]
+        if self.mode == 'hard mode':
+            non_target_birds = [x for x in self.bird_images_df[self.bird_images_df.category == target_bird_category].name.unique().tolist() if x != target_bird_name]
 
-    # Determine the index of the correct bird in the shuffled list
-    correct_bird_index = birds_to_display.index(bird_name)
+        # Determine the number of buttons to display and shuffle the order
 
-    return bird_name, bird_image, birds_to_display, correct_bird_index
+        num_buttons = min(5, len(non_target_birds))
+        multichoice_options = [target_bird_name] + random.sample(non_target_birds, num_buttons)
+        random.shuffle(multichoice_options)
+
+        correct_bird_index = multichoice_options.index(target_bird_name)
+
+        return multichoice_options, correct_bird_index
+
+    def new_game_state(self):
+        self.target_bird_name, self.target_bird_image, target_bird_category = self.get_target_bird()
+        self.multichoice_options, self.correct_bird_index = self.get_multichoice_options(self.target_bird_name, target_bird_category)
+
+        return self.target_bird_name, self.target_bird_image, self.multichoice_options, self.correct_bird_index
+
+    def post_score_results(self, selected_index):
+        if self.correct_bird_index == selected_index:
+            st.balloons()
+            self.score += 1
+        elif self.correct_bird_index != selected_index:
+            st.snow()
+            self.score -= 1
+
+    def display_multichoice(self):
+        for i, button_label in enumerate(self.multichoice_options):
+            st.button(button_label, key=i, on_click=lambda i=i: self.register_selection(i))
+
+    def register_selection(self, i):
+        self.selection = i
+    
+    def deregister_selection(self):
+        self.selection = None
+
+    def display_ui(self):
+        bird_image = f'https://cdn.download.ams.birds.cornell.edu/api/v1/asset/{self.target_bird_image}/900'
+
+        self.is_url_image(bird_image)
+
+        # Layout with two columns
+        image_column, buttons_column = st.columns([2, 1])
+
+        with image_column:
+            st.image(bird_image)
+
+        with buttons_column:
+            if self.selection is not None:
+                st.write(f'This is a {self.target_bird_name}')
+                st.button('Next', on_click=self.deregister_selection)
+
+            else:
+                self.display_multichoice()
+        
+    def is_url_image(self, image_url):
+        r = requests.head(image_url)
+        while r.status_code == 404:
+           self.new_game_state()
+
+def play_game(bird_game):
+    
+    st.title('Twitch or Tweek')
+
+    mode_previous = bird_game.mode
+    bird_game.mode = st.sidebar.radio('Difficulty', ['hard mode', 'easy mode'])
+    
+    if mode_previous != bird_game.mode:
+        bird_game.new_game_state()
+
+    st.subheader('Name that bird!')
+    bird_game.display_ui()
+
+    if bird_game.selection is not None:
+        bird_game.post_score_results(bird_game.selection)
+        bird_game.new_game_state()
+        bird_game.selection = None
+
+    st.sidebar.subheader("Your Score")
+    st.sidebar.metric("score", bird_game.score, label_visibility='collapsed')
 
 # app
-st.title('Twitch or Tweek')
-mode = st.sidebar.radio('Difficulty', ['hard mode', 'easy mode'])
-st.subheader('Name that bird!')
+if "bird_images_df" not in st.session_state:
+    st.session_state.bird_images_df = pd.read_csv('data/ebird_images.csv')
 
-if "bird_images" not in st.session_state:
-    # Load bird images DataFrame if not in session state
-    st.session_state.bird_images = pd.read_csv('data/ebird_images.csv')  # streamlit runs from the main directory rather than the subdirectory the python file is in
-if "score" not in st.session_state:
-    # Initialize the score in session state
-    st.session_state.score = 0
-if "selection_made" not in st.session_state:
-    # Initialize the selection made flag in session state
-    st.session_state.selection_made = False
-if "current_bird" not in st.session_state:
-    # Initialize current bird information in session state
-    st.session_state.current_bird, st.session_state.current_bird_image, st.session_state.buttons, st.session_state.correct_bird_index = pick_new_birds(
-        st.session_state.bird_images, mode
-    )
+if "bird_game" not in st.session_state:
+    st.session_state.bird_game = BirdGame(st.session_state.bird_images_df)
 
-# Layout with two columns
-col_image, col_buttons_1 = st.columns([2, 1])
-
-with col_image:
-    # Display bird image
-    st.image(f'https://cdn.download.ams.birds.cornell.edu/api/v1/asset/{st.session_state.current_bird_image}/1200')
-
-    if not st.session_state.selection_made:
-        with col_buttons_1:
-            # Display buttons for bird names
-            for i in range(len(st.session_state.buttons)):
-                button_label = st.session_state.buttons[i]
-                buttons = st.button(button_label)
-
-                # Check if correct bird button is pressed
-                if buttons and st.session_state.correct_bird_index == i:
-                    st.balloons()  # Celebrate correct answer
-                    st.session_state.score += 1
-                    st.session_state.selection_made = True
-                elif buttons and st.session_state.correct_bird_index != i:
-                    st.snow()  # Indicate incorrect answer
-                    st.session_state.score -= 1
-                    st.session_state.selection_made = True
-
-if st.session_state.selection_made:
-    # Display correct bird name after selection is made
-    st.write(f'This is a {st.session_state.current_bird}')
-    next_button = st.button('Next')
-    # Load new bird information for the next round
-    st.session_state.current_bird, st.session_state.current_bird_image, st.session_state.buttons, st.session_state.correct_bird_index = pick_new_birds(
-        st.session_state.bird_images, mode
-    )
-    st.session_state.selection_made = False
-
-# Display score in the sidebar
-st.sidebar.subheader("Your Score")
-st.sidebar.metric("score", st.session_state.score, label_visibility='collapsed')
+play_game(st.session_state.bird_game)
