076fe9b06e7c4714f60c29050234b7b2ffb4925e
megan.evans <meganroseevans1@gmail.com>
2024-02-15 13:30:17 +0000

    adding audio file capability and copyright referencing

--- data/crawlers/get_bird_images.js
+++ data/crawlers/get_bird_images.js
@@ -1,6 +1,7 @@
 /* A script for retrieving bird names and images from eBird
 
-- Go to  https://media.ebird.org/catalog?regionCode=AU&sort=rating_rank_desc&mediaType=photo
+- For Photos, go to  https://media.ebird.org/catalog?regionCode=AU&sort=rating_rank_desc&mediaType=photo
+- For Audio, go to  https://media.ebird.org/catalog?regionCode=AU&sort=rating_rank_desc&mediaType=audio
 - Click 'More Results' a bunch of times
 - Inspect page, go to Console, run the below code
 - Save output by right clicking on object, copy and paste into new file called 'get_bird_images_output.json'
@@ -15,10 +16,11 @@ function collectMatchingImageAttributes() {
 
   const matchingImages = Array.from(images).filter((img) => img.src.startsWith(prefix)).map((fimg) => {
     const alt = fimg.alt.split(' - ')[0].trim();
+    const cc = fimg.alt.split(' - ')[1].trim();
     const srcParts = fimg.src.split('/');
     const src = srcParts[srcParts.length - 2];
 
-    return { alt, src };
+    return { alt, cc, src };
   });
 
   return matchingImages;
