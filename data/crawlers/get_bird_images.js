f401622c72f30cb24ca2e00d6fca4e8f0c5135a1
megan.evans <meganroseevans1@gmail.com>
2025-06-01 21:49:50 +1000

    add wa and tx, clean up others

--- data/crawlers/get_bird_images.js
+++ data/crawlers/get_bird_images.js
@@ -1,17 +1,14 @@
 /* A script for retrieving bird names and images from eBird
 
 - For Photos, go to  https://media.ebird.org/catalog?regionCode=AU&sort=rating_rank_desc&mediaType=photo
-- For Audio, go to  https://media.ebird.org/catalog?regionCode=AU&sort=rating_rank_desc&mediaType=audio
 - Click 'More Results' a bunch of times
 - Inspect page, go to Console, run the below code
-- Save output by right clicking on object, copy and paste into new file called 'get_bird_images_output.json'
-- To convert it from a json to csv, run `$ jq -r '.[] | [.name, .image_id] | @csv' get_bird_images_output.json > get_bird_images_output.csv
-- To add these new images to the existing image database, run `$ cat images.csv <(echo) get_bird_images_output.csv > images.csv`
-- To remove duplicates, run `$ sort -u images.csv -o images.csv`
+- Save output by right clicking on object, copy and paste into new file called 'new_images.json'
+- To convert it from a json to csv, run `$ jq -r '.[] | [.name, .cc, .image_id] | @csv' new_images.json > new_images.csv
 */
 
 function collectMatchingImageAttributes() {
-  const prefix = 'https://cdn.download.ams.birds.cornell.edu/api/v1/asset/';
+  const prefix = 'https://cdn.download.ams.birds.cornell.edu/api/v2/asset/';
   const images = document.querySelectorAll('img');
 
   const matchingImages = Array.from(images).filter((img) => img.src.startsWith(prefix)).map((fimg) => {
